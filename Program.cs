fac496c9299e883a325405d9766e3b796386ef2e
AlexandrBelyakov <belya0309@mail.ru>
2023-03-26 18:58:58 +0300

    переиминовали

--- Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.Clear();
-Console.Write("Введите число:");
-int number;
-number = int.Parse(Console.ReadLine());
-int sqr = number * number;
-Console.WriteLine($"Квадрат числа {number} равен {sqr}");
-int sqr1 = Convert.ToInt32(Math.Pow(number, 3));
-Console.WriteLine($"Квадрат числа {number} равен {sqr1}");
